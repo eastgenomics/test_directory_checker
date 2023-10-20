5ee94e982b78a318de85b98ee458b7a906637b80
Yujin Kim <yujin.kim@hotmail.fr>
2023-10-20 12:02:55 +0100

    new test for checking presence in db + fixing compare gp td test

--- tests/test_checker.py
+++ tests/test_checker.py
@@ -150,6 +150,10 @@ def test_compare_gp_td(
         panel dictionary
     """
 
+    gene_locus_type = utils.get_locus_status_genes(
+        setup_td_data, setup_signedoff_panels, setup_hgnc_dump
+    )
+
     expected_identical_tests = pd.DataFrame(
         [
             [
@@ -171,8 +175,8 @@ def test_compare_gp_td(
                 "HGNC:3531_SG_panel_1.0.0, HGNC:3534_SG_panel_1.0.0",
                 "HGNC:3531, HGNC:3534", "R122.1",
                 "F13A1, F13B, F2", "",
-                "HGNC:3531, HGNC:3534, HGNC:3535",
-                None, "HGNC:3535"
+                "HGNC:3531, HGNC:3534, HGNC:6666",
+                None, "HGNC:6666"
             ],
             [
                 "R143.1_Neonatal diabetes_P",
@@ -244,62 +248,11 @@ def test_compare_gp_td(
         ]
     )
 
-    expected_all_tests = pd.DataFrame(
-        [
-            [
-                "R100.2",
-                "Test panel",
-                "HGNC:10471, HGNC:11730, HGNC:11998, HGNC:1349, HGNC:1833, HGNC:18674, HGNC:29186, HGNC:3495, HGNC:4171, HGNC:6919",
-            ],
-            [
-                "R122.1",
-                "F13A1, F13B, F2",
-                "HGNC:3531, HGNC:3534, HGNC:3535",
-            ],
-            [
-                "R130.1",
-                "Short QT syndrome (224)",
-                "HGNC:1390, HGNC:6251, HGNC:6263, HGNC:6294",
-            ],
-            [
-                "R134.2",
-                "Familial hypercholesterolaemia",
-                "HGNC:18640, HGNC:20001, HGNC:603, HGNC:613, HGNC:6547",
-            ],
-            [
-                "R134.3",
-                "Familial hypercholesterolaemia",
-                "HGNC:1228",
-            ],
-            [
-                "R143.1",
-                "ABCC8",
-                "HGNC:59"
-            ],
-            [
-                "R341.1",
-                "SERPING1",
-                "HGNC:1228"
-            ],
-            [
-                "R347.1",
-                "Inherited predisposition to acute myeloid leukaemia (AML) (525)",
-                "HGNC:10471, HGNC:11730, HGNC:11998, HGNC:1349, HGNC:1833, HGNC:18674, HGNC:29186, HGNC:3495, HGNC:4171, HGNC:6919",
-            ],
-            [
-                None, None, None
-            ]
-        ],
-        columns=[
-            "td_ci", "td_target", "td_genes"
-        ]
-    )
-
     (
-        identical_tests, removed_tests, replaced_tests, all_tests
+        identical_tests, removed_tests, replaced_tests
     ) = checker.compare_gp_td(
-        setup_td_data, setup_genepanels_data, setup_hgnc_dump,
-        setup_signedoff_panels
+        setup_td_data, setup_genepanels_data, setup_signedoff_panels,
+        gene_locus_type
     )
 
     for col in identical_tests.columns:
@@ -318,12 +271,6 @@ def test_compare_gp_td(
         np.testing.assert_array_equal(
             replaced_tests[col].to_numpy(),
             expected_replaced_tests[col].to_numpy()
-        )
-
-    for col in all_tests.columns:
-        np.testing.assert_array_equal(
-            all_tests[col].to_numpy(),
-            expected_all_tests[col].to_numpy()
         )
 
     # not part of the actual test but to provide examples in the readme
@@ -373,13 +320,13 @@ def test_find_new_clinical_indications(setup_td_data, setup_genepanels_data):
                 "Single gene sequencing <=10 amplicons", "set()"
             ],
             [
-                "R500.1", "Test new ci", "Test new panel", ["100"],	[],
+                "R500.1", "Test new ci", "Test new panel", ["525"],	[],
                 "Small panel", "set()"
             ],
             [
-                "R666.1", "Test new genes", "Test new genes", [],
-                ["HGNC:483858"], "Single gene sequenceing <=10 amplicons",
-                "set()"
+                "R666.1", "Test no clinical transcript",
+                "Test no clinical transcript", [], ["HGNC:9999"],
+                "Single gene sequencing <=10 amplicons", "set()"
             ]
         ],
         columns=[
@@ -400,16 +347,53 @@ def test_find_new_clinical_indications(setup_td_data, setup_genepanels_data):
 
 
 def test_check_if_genes_in_db(
-    setup_td_data, setup_genepanels_data, setup_hgnc_dump,
-    setup_signedoff_panels
+    setup_td_data, setup_hgnc_dump, setup_signedoff_panels
 ):
-    (
-        identical_tests, removed_tests, replaced_tests, all_tests
-    ) = checker.compare_gp_td(
-        setup_td_data, setup_genepanels_data, setup_hgnc_dump,
-        setup_signedoff_panels
+    gene_locus_type = utils.get_locus_status_genes(
+        setup_td_data, setup_signedoff_panels, setup_hgnc_dump
+    )
+
+    genes_to_check = utils.get_genes_from_td_target(
+        setup_td_data, setup_signedoff_panels, gene_locus_type
     )
 
     presence_in_db_df = checker.check_if_genes_present_in_db(
-        "", "", "tests/test_files/test_db.db", "sqlite", all_tests["td_genes"]
+        "", "", "tests/test_files/test_db.db", "sqlite", genes_to_check
     )
+
+    expected_presence_genes = pd.DataFrame(
+        [
+            ["HGNC:10471", True, True],
+            ["HGNC:11730", True, True],
+            ["HGNC:11998", True, True],
+            ["HGNC:1228", True, True],
+            ["HGNC:1349", True, True],
+            ["HGNC:1390", True, True],
+            ["HGNC:1833", True, True],
+            ["HGNC:18640", True, True],
+            ["HGNC:18674", True, True],
+            ["HGNC:20001", True, True],
+            ["HGNC:29186", True, True],
+            ["HGNC:3495", True, True],
+            ["HGNC:3531", True, True],
+            ["HGNC:3534", True, True],
+            ["HGNC:4171", True, True],
+            ["HGNC:59", True, True],
+            ["HGNC:603", True, True],
+            ["HGNC:613", True, True],
+            ["HGNC:6251", True, True],
+            ["HGNC:6263", True, True],
+            ["HGNC:6294", True, True],
+            ["HGNC:6547", True, True],
+            ["HGNC:6666", False, False],
+            ["HGNC:6919", True, True],
+            ["HGNC:9999", True, False]
+        ],
+        columns=["gene", "presence_in_db", "has_clinical_transcript"]
+    )
+
+    for col in presence_in_db_df.columns:
+        np.testing.assert_array_equal(
+            presence_in_db_df[col].to_numpy(),
+            expected_presence_genes[col].to_numpy()
+        )
